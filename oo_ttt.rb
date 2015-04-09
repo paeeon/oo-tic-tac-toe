6d14172c22a15e5d3e015b193ac0fefdd9f11069
Lily Jen <lily@lilyjen.com>
2015-04-09 10:22:52 -0400

    Adds more logic

--- oo_ttt.rb
+++ oo_ttt.rb
@@ -1,31 +1,49 @@
+require 'pry'
+
 class Player
+  attr_accessor :spot_selection
+
+  def fill_spot(board, player, fill_content)
+    board.grid_spots[player.spot_selection] = fill_content
+  end
 end
 
 class Human < Player
-  def choose_a_spot
+  def choose_a_spot(board)
     begin
       puts "Pick a square from 1-9."
-      spot_selection = gets.chomp
-    end until %w(1 2 3 4 5 6 7 8 9).include?(spot_selection)
-    spot_selection.to_i
+      self.spot_selection = gets.chomp
+    end until %w(1 2 3 4 5 6 7 8 9).include?(self.spot_selection)
+    until board.remaining_empty_spots.map{|number| number.to_s}.include?(spot_selection)
+      binding.pry
+      puts "That spot's already taken! Pick another square."
+      self.spot_selection = gets.chomp
+    end 
+    self.spot_selection = self.spot_selection.to_i
+    board.grid_spots[self.spot_selection] = 'X'
+    board.remaining_empty_spots.delete(self.spot_selection)
   end
 end
 
 class Computer < Player
-  def choose_a_spot
+  def choose_a_spot(board)
+    self.spot_selection = board.remaining_empty_spots.sample.to_i
+    board.grid_spots[self.spot_selection] = 'O'
+    board.remaining_empty_spots.delete(self.spot_selection)
   end
 end
 
 class Board
+  WINNING_LINES = [[1,2,3],[1,5,9],[1,4,7],[2,5,8],[3,6,9],[3,5,7],[4,5,6],[7,8,9]]
   attr_accessor :grid_spots
   attr_reader :remaining_empty_spots
 
   def initialize
     @grid_spots = []
     @remaining_empty_spots = []
-    0..9.times do |time|
-      grid_spots[time] = " "
-      remaining_empty_spots << time
+    for i in 1..9
+      grid_spots[i] = " "
+      remaining_empty_spots << i
     end
   end
 
@@ -46,20 +64,46 @@ class Board
 end
 
 class Game
-  attr_accessor :board, :human, :computer
+  attr_accessor :board, :human, :computer, :winner
 
   def initialize
     @board = Board.new
     @human = Human.new
     @computer = Computer.new
+    @winner = nil
+  end
+
+  def check_for_winner
+    Board::WINNING_LINES.each do |line|
+      if line.select {|num| board.grid_spots[num] == 'X'}.length == 3
+        binding.pry
+        winner = "you"
+      elsif line.select {|num| board.grid_spots[num] == 'O'}.length == 3
+        winner = "computer"
+      end
+    end
+  end
+
+  def declare_winner
+    puts "The winner is #{winner}!"
   end
 
   def play
     board.draw_board
     puts "Welcome to Tic-Tac-Toe!"
-    board.grid_spots[(human.choose_a_spot).to_i] = 'X'
-    board.draw_board
-    #computer.choose_a_spot
+    begin
+      human.choose_a_spot(board)
+      computer.choose_a_spot(board)
+      human.fill_spot(board, human, 'X')
+      self.check_for_winner
+      break if winner
+      #binding.pry
+      computer.fill_spot(board, computer, 'O')
+      self.check_for_winner
+      break if winner
+      board.draw_board
+    end until winner != nil
+    self.declare_winner
   end
 
 end
